ad17ffd154c8b0abdea2db1f29f3dda84bdd8d83
Jostein Berre Eliassen <spam@jostein.be>
2017-09-18 05:02:19 -0400

    various, fzf

--- global/zsh/plugins.zsh
+++ global/zsh/plugins.zsh
@@ -1,7 +1,6 @@
 antigen use prezto
 antigen bundle git
 antigen bundle olivierverdier/zsh-git-prompt
-antigen bundle zsh-users/zsh-syntax-highlighting
 antigen bundle zsh-users/zsh-completions
 antigen bundle zsh-users/zsh-history-substring-search
 antigen apply
